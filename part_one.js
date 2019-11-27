2f6f00f553951aa855eba9dc9383797061319a77
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2019-11-27 15:44:43 +0100

    Börjat på call function

--- part_one.js
+++ part_one.js
@@ -1,18 +1,27 @@
 var myObject = {
     prototypes:[]
 }
-myObject.call = function (func) {
-    console.log("call funktion")
-    this.hasOwnProperty(func)
-}
 
 myObject.create = function (prototypeList){
     var newObject = {};
     newObject.__proto__ = this;
     newObject.prototypes = prototypeList;
-
-
     return newObject;
 }
 
+myObject.call = function (funcName, parameters) {
+    console.log("In call function");
 
+    if(typeof this[funcName] === "function"){
+        console.log("In if statemt");
+        return this[funcName](parameters);
+    }
+}
+
+o1 = myObject.create(null)
+
+o1.func =  function(Parameters){
+    return "fun funktionen"
+}
+
+o1.call("func", "Parameters")
