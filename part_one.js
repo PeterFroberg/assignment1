a2338942c54b2c8acbd3c7fbe538cc6d200b0413
peter.froberg@stenhamra.se <jtb10lNHVJ8>
2019-11-28 14:30:22 +0100

    PART ON complete

--- part_one.js
+++ part_one.js
@@ -20,31 +20,31 @@ myObject.call = function (funcName, parameters) {
         return this[funcName](parameters);
     }
     if (this.prototypes != null) {
-        for (parentresult of this.prototypes) {
-            var parentresult = parentresult.call(funcName, parameters);
+        for (parent of this.prototypes) {
+            var parentresult = parent.call(funcName, parameters);
             if (parentresult) {
                 return parentresult;
             }
         }
-    }return null;
+    }
+    return null;
 }
-
 
 
 //Kurstester *****************
 var obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
+obj0.func = function (arg) {return "func0: " + arg;};
 var obj1 = myObject.create([obj0]);
 var obj2 = myObject.create([]);
-obj2.func = function(arg) { return "func2: " + arg; };
+obj2.func = function (arg) {return "func2: " + arg;};
 var obj3 = myObject.create([obj1, obj2]);
-var result = obj3.call("func", ["hello"]) ;
+var result = obj3.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
 
 
 //Second test
 obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
+obj0.func = function (arg) {return "func0: " + arg;};
 obj1 = myObject.create([obj0]);
 obj2 = myObject.create([]);
 obj3 = myObject.create([obj2, obj1]);
@@ -53,6 +53,6 @@ console.log("should print ’func0: hello’ ->", result);
 
 //Third test
 obj0 = myObject.create(null);
-obj0.func = function(arg) { return "func0: " + arg; };
+obj0.func = function (arg) {return "func0: " + arg;};
 result = obj0.call("func", ["hello"]);
 console.log("should print ’func0: hello’ ->", result);
